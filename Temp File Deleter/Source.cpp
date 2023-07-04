c0928f3f803820abdee4236e23638edfc18334fb
Commander_Rourke <96107413+GoldNR@users.noreply.github.com>
2023-07-04 23:42:43 +0800

    Update Source.cpp
    minor changes, and a "cin.get()" at the end so the console doesn't close automatically

--- Temp File Deleter/Source.cpp
+++ Temp File Deleter/Source.cpp
@@ -3,44 +3,38 @@
 
 namespace fs = std::filesystem;
 
-void deleteFolder(const fs::path& folderPath)
-{
-    for (const auto& entry : fs::directory_iterator(folderPath))
-    {
-        try
-        {
-            if (fs::is_directory(entry))
-            {
+void deleteFolder(const fs::path& folderPath) {
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        try {
+            if (fs::is_directory(entry)) {
                 deleteFolder(entry.path());
             }
-            else
-            {
+            else {
                 fs::remove(entry);
             }
         }
-        catch (const fs::filesystem_error& ex)
-        {
-            std::cerr << "Error deleting file: " << entry.path().string() << '\n';
-            std::cerr << "Reason: " << ex.what() << '\n';
+        catch (const fs::filesystem_error& ex) {
+            std::cerr << "Error deleting file: " << entry.path().string() << std::endl;
+            std::cerr << "Reason: " << ex.what() << std::endl << std::endl;
         }
     }
 
     fs::remove(folderPath);
 }
 
-int main()
-{
+int main() {
     const fs::path tempFolderPath = fs::temp_directory_path();
 
-    try
-    {
+    try {
         deleteFolder(tempFolderPath);
-        std::cout << "All files and folders in the temp folder have been deleted.\n";
+        std::cout << "All files and folders in the temp folder have been deleted." << std::endl << std::endl;
     }
-    catch (const std::exception& ex)
-    {
-        std::cerr << "Error: " << ex.what() << '\n';
+    catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << std::endl << std::endl;
     }
+
+    std::cout << "Press Enter to exit...";
+    std::cin.get();
 
     return 0;
 }
